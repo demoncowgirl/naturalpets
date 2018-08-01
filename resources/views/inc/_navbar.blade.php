aa330843b34335f4b9d9c35929a857676b2bbd2d
Beth <bethsalvatore2018@gmail.com>
2018-08-01 09:04:08 -0400

    fixed navbar

--- resources/views/inc/_navbar.blade.php
+++ resources/views/inc/_navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar-inverse navbar-toggleable-md" style="border: none; height: 110px;">
+<nav class="navbar-inverse navbar-toggleable-md navbar-expand-md" style="border: none; height: 110px;">
   <div class = "container-fluid">
     <div class="navbar-header">
       <a class ="navbar-brand" href="{{ url('/') }}">
@@ -9,30 +9,36 @@
       </a>
   </div>
 
-  <ul class="nav navbar-nav">
-       <!-- Authentication Links -->
-       @guest
-      <li><a href="{{ route('login') }}">Login</a></li>
-      <li><a href="{{ route('register') }}">Register</a></li>
-       @else
-           <a class="nav-link" v-pre>
-               {{ Auth::user()->name }} <span class="caret"></span>
-           </a>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Logout<span class="caret"></span></a>
-        <ul class="dropdown-menu">
-           <li><a href="{{ route('logout') }}"
-                onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">Logout</a></li>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                       {{ csrf_field() }}
-            </form>
-       @endguest
+    <ul class="nav navbar-nav float-right">
         <li><a class="{{ Request::is ('/') ? 'active': ''}}" href="/">Home</a></li>
         <li><a class="{{ Request::is ('about') ? 'active': ''}}" href="/about">About</a></li>
         <li><a class="{{ Request::is ('contact') ? 'active': ''}}" href="/contact">Contact</a></li>
         <li><a class="{{ Request::is ('profile') ? 'active': ''}}" href="/profile">Profile</a></li>
         <li><a class="{{ Request::is ('petSearch') ? 'active': ''}}" href="/petSearch">Pet Search</a></li>
         <li><a class="{{ Request::is ('serviceSearch') ? 'active': ''}}" href="/serviceSearch">Pet Services</a></li>
-      </ul>
-    </div>
-</nav>
+
+       <!-- Authentication Links -->
+       @guest
+      <li class="nav-item">
+        <a class ="nav-link" href="{{ route('login') }}">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class ="nav-link" href="{{ route('register') }}">Register</a>
+      </li>
+    </ul>
+       @else
+           <a class="nav-link" v-pre>
+               {{ Auth::user()->name }} <span class="caret"></span>
+           </a>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Logout<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+           <li><a class ="nav-link" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">Logout</a></li>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                       {{ csrf_field() }}
+            </form>
+       @endguest
+
+  </nav>
+</div>
