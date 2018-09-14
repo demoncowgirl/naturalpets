e7527e0f3f75b65f5592fbf150d9dec0bd4f494f
Beth <bethsalvatore2018@gmail.com>
2018-09-13 21:46:10 -0400

    merge

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,58 +3,115 @@
 @section('content')
 <h1>Profile</h1>
 <div id="profileForm" class="container">
-{!! Form::open(['url' => '/profile']) !!}
-  <div class="form-group" method="post" action="{{htmlspecialchars($_SERVER["PHP_SELF"])}}">
-    {{ Form::label('name', '',['class' => 'form-control', 'placeholder' => 'Enter Name'] ) }}
-    {{ Form::text('name', 'Name') }}
+  <form>
+   <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="username">Email</label>
+      <input type="email" class="form-control" id="username" placeholder="Username">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="password">Password</label>
+      <input type="password" class="form-control" id="password" placeholder="Password">
+    </div>
   </div>
-  <div class="form-group" method="post" action="{{htmlspecialchars($_SERVER["PHP_SELF"])}}">
-    {{ Form::label('email', '',['class' => 'form-control', 'placeholder' => 'Enter E-mail'] ) }}
-    {{ Form::text('email', 'E-Mail Address') }}
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="first_name">First Name</label>
+      <input type="text" class="form-control" id="first_name" placeholder="First name">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="last_name">Last Name</label>
+      <input type="last_name" class="form-control" id="last_name" placeholder="Last name">
+    </div>
   </div>
-  <div class="form-group" method="post" action="{{htmlspecialchars($_SERVER["PHP_SELF"])}}">
-    {{ Form::label('address', '',['class' => 'form-control', 'placeholder' => 'Enter Address'] ) }}
-    {{ Form::text('address', 'Address') }}
+  <div class="form-row">
+  <div class="form-group col-md-12">
+    <label for="street_address">Street Address</label>
+    <input type="text" class="form-control" id="street_address" placeholder="Street address">
   </div>
-  <div class="form-group" method="post" action="{{htmlspecialchars($_SERVER["PHP_SELF"])}}">
-    {{ Form::label('city', '',['class' => 'form-control', 'placeholder' => 'Enter City'] ) }}
-    {{ Form::text('city', 'City') }}
   </div>
-  <div class="form-group" method="post" action="{{htmlspecialchars($_SERVER["PHP_SELF"])}}">
-    {{ Form::label('state', '',['class' => 'form-control', 'placeholder' => 'State'] ) }}
-    {{ Form::text('state', 'State') }}
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCity">City</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputState">State</label>
+      <select id="inputState" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Zip</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
   </div>
-  <div class="form-group" method="post" action="{{htmlspecialchars($_SERVER["PHP_SELF"])}}">
-    {{ Form::label('zipCode', '',['class' => 'form-control', 'placeholder' => 'zipCode'] ) }}
-    {{ Form::text('zipcode', 'ZipCode') }}
+
+  <div class="form-row">
+    <div class="form-group col-md-2">
+    <label for="family_size">Family Size</label>
+    <select name="family_size">
+      <option value="#" >1-2</option>
+      <option value="#" >3-5</option>
+      <option value="#" >6-9</option>
+      <option value="#" >10 or more</option>
+    </select>
+    </div>
+    <div class="form-group col-md-2">
+    <label for="housing">Housing</label>
+      <div class="checkbox">
+        <label><input type="checkbox" value="own">Own</label>
+      </div>
+      <div class="checkbox">
+        <label><input type="checkbox" value="rent">Rent</label>
+      </div>
+      <div class="checkbox disabled">
+        <label><input type="checkbox" value="other">Other</label>
+      </div>
+    </div>
+    <div class="form-group col-md-2">
+    <label for="fence">Fenced Yard?</label>
+      <div class="checkbox">
+        <label><input type="checkbox" value="yes">Yes</label>
+      </div>
+      <div class="checkbox">
+        <label><input type="checkbox" value="no">No</label>
+      </div>
+    </div>
+
+  <div class="form-group col-md-1">
+    <label for="pet">Pets</label>
+    <div class="checkbox">
+      <label><input type="checkbox" value="dog">Dog</label><br>
+      <label><input type="checkbox" value="cat">Cat</label><br>
+      <label><input type="checkbox" value="bird">Bird</label><br>
+      <label><input type="checkbox" value="horse">Horse</label>
+    </div>
   </div>
-  <div class="form-group" method="post" action="{{htmlspecialchars($_SERVER["PHP_SELF"])}}">
-    {{ Form::label('Family Size', '',['class' => 'form-control', 'placeholder' => 'Family Size'] ) }}
-    {{ Form::text('num_in_family', 'Family Size') }}
+    <div class="form-group col-md-3">
+      <div class ="checkbox">
+      <label><input type="checkbox" value="barnyard">Barnyard</label><br>
+      <label><input type="checkbox" value="reptile">Reptile</label><br>
+      <label><input type="checkbox" value="smallfurry">Small Furry</label>
+    </div>
   </div>
-  <!-- <div class="form-group" method="post" action="{{htmlspecialchars($_SERVER["PHP_SELF"])}}">
-    {{ Form::label('apt_or_house', '',['class' => 'form-control'] ) }}
-      {{ Form::checkbox('apt_or_house', 'house' , true) }}
-      {{ Form::checkbox('apt_or_house', 'apartment' , false) }}
+    <div class="form-group col-md-5 m-2">
+      <form action="/action_page.php">
+        <input type="file" name="pic" accept="image/*">
+        <button type="submit" class="btn-sm btn-outline-secondary m-2 pull-left">Upload</button>
+      </form>
+    </div>
+    <div class="form-group col-md-4">
+      <button type="submit" class="btn btn-dark m-0 btn-md" style="center">Submit</button>
+    </div>
+    <div class="form-group col-md-2">
+      <div id="retriever" class = "row">
+      @include('inc._retriever_wagging')
+      </div>
+    </div>
   </div>
-  <div>
-    {{ Form::label('fenced_yard', '',['class' => 'form-control'] ) }}
-      {{ Form::checkbox('fenced_yard', 'yes', true) }}
-      {{ Form::checkbox('fenced_yard', 'no', false) }}
-  </div> -->
-  <div>
-    <!-- todo: add subform with the following:
-          pets_in_family smallint
-          pet_photo upload
-          pet_breed
-          age_of_pet -->
-    <!-- {{ Form::submit ('Submit', ['class' => 'btn btn-dark m-2']) }} -->
-    {{ Form::submit ('Submit', ['class' => 'btn btn-dark m-0 btn disabled', 'disabled' => true]) }}
-  </div>
-{!! Form::close() !!}
-<div>
-</div>
-<div id="terrier" class = "row">
-@include('inc._terriergif')
+
+
 </div>
 @endsection
