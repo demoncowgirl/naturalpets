f760763eb3b63686fbf998da28abb4570d749f2b
demoncowgirl <bethsalvatore2018@gmail.com>
2019-02-14 22:02:35 -0500

    adds PostsSeeder

--- database/migrations/2019_02_13_170456_add_slug_to_posts_table.php
+++ database/migrations/2019_02_13_170456_add_slug_to_posts_table.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-class AddSlugToPosts extends Migration
+class AddSlugToPostsTable extends Migration
 {
     /**
      * Run the migrations.
